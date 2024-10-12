8deeffdb0a27fff3a261f8031433a34767b0aabc
花茉 <xiangyueluo@gmail.com>
2024-10-13 00:50:33 +0800

    feat: 增加了oled支持 增加了ws2812 rgb_matrix支持

--- 17/keymaps/vial/keymap.c
+++ 17/keymaps/vial/keymap.c
@@ -2,23 +2,17 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT (
-        KC_NUM  ,KC_P7   ,KC_P8          ,KC_P9   ,KC_MUTE ,
+        KC_MUTE ,                                  KC_MUTE ,
+        KC_NUM  ,KC_P7   ,KC_P8          ,KC_P9   ,
         KC_PSLS ,KC_P4   ,KC_P5          ,KC_P6   ,KC_PPLS ,
         KC_PAST ,KC_P1   ,KC_P2          ,KC_P3   ,
         KC_PMNS ,KC_P0   ,LT(1,KC_PDOT)  ,KC_PENT
-    ),
-    [1] = LAYOUT (
-        RGB_HUI ,RGB_TOG ,RGB_VAI ,XXXXXXX  ,XXXXXXX,
-        RGB_HUD ,RGB_MOD ,KC_A    ,RGB_RMOD ,XXXXXXX,
-        RGB_SAI ,RGB_SPI ,RGB_VAD ,RGB_SPD  ,
-        RGB_SAD ,XXXXXXX ,XXXXXXX ,XXXXXXX
     )
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [1] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)}
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU),ENCODER_CCW_CW(KC_VOLD, KC_VOLU)}
 };
 #endif
 
@@ -42,9 +36,19 @@ bool rgb_matrix_indicators_kb(void) {
 
 
 
-#if defined(OLED_ENABLE)
+#ifdef OLED_ENABLE
+static void render_logo(void) {
+    static const char PROGMEM qmk_logo[] = {
+        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+    };
+
+    oled_write_P(qmk_logo, false);
+}
+
 bool oled_task_user(void) {
-    oled_write_P(PSTR("Hello World! "), false);
-    return false;
+    render_logo();
+    return true;
 }
 #endif
--- 17/rules.mk
+++ 17/rules.mk
@@ -1,10 +1,8 @@
-MCU = STM32F103
-BOOTLOADER = vibl
-
-
-UNICODE_ENABLE = yes         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-# LTO_ENABLE = yes
+UNICODE_ENABLE=yes         # Unicode
+# BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+LTO_ENABLE=yes
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
-OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+# OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+
+OLED_ENABLE=yes
