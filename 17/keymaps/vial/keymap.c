0bd84cc62e98b3268577e7efde931bff7a090223
花茉 <xiangyueluo@gmail.com>
2024-10-13 02:05:22 +0800

    feat: 精简固件

--- 17/keymaps/vial/keymap.c
+++ 17/keymaps/vial/keymap.c
@@ -16,39 +16,54 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 };
 #endif
 
-#if defined(RGB_MATRIX_ENABLE)
-bool rgb_matrix_indicators_kb(void) {
-    if (!rgb_matrix_indicators_user()) {
-        return false;
-    }
-    if(host_keyboard_led_state().num_lock) {	//处于 NUM LOCK 状态
-		rgb_matrix_set_color(0, 255, 0, 0);	//把序号0的灯变为红色（R:255, G:0, B:0）
-	}
-	if(get_highest_layer(layer_state) == 1) {	//如果在“1”层（Fn层）
-		if(HAS_FLAGS(g_led_config.flags[15], 0x04)) {	//若该灯是轴座灯
-			rgb_matrix_set_color(15, 153, 238, 255);			//颜色变为冰蓝色
-		}
-	}
-    return true;
-}
-#endif
-//
-
+// #if defined(RGB_MATRIX_ENABLE)
+// bool rgb_matrix_indicators_kb(void) {
+//     if (!rgb_matrix_indicators_user()) {
+//         return false;
+//     }
+//     if(host_keyboard_led_state().num_lock) {	//处于 NUM LOCK 状态
+// 		rgb_matrix_set_color(0, 255, 0, 0);	//把序号0的灯变为红色（R:255, G:0, B:0）
+// 	}
+// 	if(get_highest_layer(layer_state) == 1) {	//如果在“1”层（Fn层）
+// 		if(HAS_FLAGS(g_led_config.flags[15], 0x04)) {	//若该灯是轴座灯
+// 			rgb_matrix_set_color(15, 153, 238, 255);			//颜色变为冰蓝色
+// 		}
+// 	}
+//     return true;
+// }
+// #endif
+// //
 
 
 #ifdef OLED_ENABLE
-static void render_logo(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
-    };
-
-    oled_write_P(qmk_logo, false);
-}
-
 bool oled_task_user(void) {
-    render_logo();
-    return true;
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case 0:
+            oled_write_P(PSTR("Default\n"), false);
+            break;
+        case 1:
+            oled_write_P(PSTR("FN\n"), false);
+            break;
+        case 2:
+            oled_write_P(PSTR("2\n"), false);
+            break;
+        case 3:
+            oled_write_P(PSTR("3\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+
+    return false;
 }
 #endif
--- 17/rules.mk
+++ 17/rules.mk
@@ -1,8 +1,15 @@
-UNICODE_ENABLE=yes         # Unicode
-# BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-LTO_ENABLE=yes
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = yes      # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
+MIDI_ENABLE = no            # MIDI support
+UNICODE_ENABLE = no         # Unicode
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+AUDIO_ENABLE = no           # Audio output on port C6
+FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
+LTO_ENABLE = yes
+OLED_ENABLE=yes
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
-# OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
-
-OLED_ENABLE=yes
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
